dec2e1387f911432bf0238c966401475f1e60212
Yuvraj Mishra <40194399+VanillaBase1lb@users.noreply.github.com>
2022-09-11 16:25:53 +0530

    add requirements and format file

--- ui/window.py
+++ ui/window.py
@@ -1,14 +1,27 @@
-import tkinter as tk
-from tkinter import Label, Button, filedialog
 import tkinter
-window=tkinter.Tk()
+import tkinter as tk
+from tkinter import Button, Label, filedialog
+
+window = tkinter.Tk()
 window.title("File Browser")
 window.geometry('700x500')
+
+
 def open_file():
     file = filedialog.askopenfilename(filetypes=[("All Files", "*.*")])
     print(file)
-bt1=Button(window, text="Input Path",bg="green", fg="white", command=open_file)
-bt1.grid(column=0,row=0)
-bt2=Button(window, text="Output Path",bg="green", fg="white", command=open_file)
-bt2.grid(column=0,row=2)
+
+
+bt1 = Button(window,
+             text="Input Path",
+             bg="green",
+             fg="white",
+             command=open_file)
+bt1.grid(column=0, row=0)
+bt2 = Button(window,
+             text="Output Path",
+             bg="green",
+             fg="white",
+             command=open_file)
+bt2.grid(column=0, row=2)
 window.mainloop()
